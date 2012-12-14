aaff63be96a5d4faac22c192813191500fb7eea8
Gleb Peregud <gleber.p@gmail.com>
2012-12-14 21:09:09 +0100

    improve report generating script

--- show.R
+++ show.R
@@ -21,6 +21,47 @@ if (!exists("results")) {
 ## Main loop
 ##
 
+## 1. function number
+## 2. dimentions, only c(1,30) are used
+## 3. run
+## 4. function evals barriers
+## 5. values
+##    1: value
+##    2: iterations
+##    3: number of f.eval
+
+## results
+## table 10 dimensions
+##   bigrow for each barrier
+##      rows quantiles, mean, std
+##   columns
+##      function numbers
+
+print(dim(results))
+
+res.rows = 0 # 7*length(dims)*length(fes)
+res.cols = length(funcs)
+res = matrix(nrow=res.rows, ncol=res.cols)
+
+print(res)
+## stop()
+
+biases = t(as.matrix(fbias))[,rep(1,25)]
+biases = biases[funcs,]
+
+for (d in dims) {
+  indim = results[funcs,d,,,1]
+  for (fe.i in 1:length(fes)) {
+    vals = indim[,,fe.i]
+    infe = vals - biases
+    infe.s = apply(infe, 1, function(x) c(quantile(x, na.rm=TRUE),mean(x),sd(x)))
+    res = rbind(res, infe.s)
+  } 
+}
+print(res)
+
+stop()
+
 for (i in funcs) { # for each of desired functions
   limit = limits[i,]
   bias = fbias[i]
